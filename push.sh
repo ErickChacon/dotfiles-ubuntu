dcc0c25ccae5b096f8bf21e1d06cd8765afe8a2f
Erick Chacon <eral.th07@gmail.com>
2017-07-23 16:15:15 +0100

    push

--- push.sh
+++ push.sh
@@ -13,7 +13,7 @@ cp /etc/R/Rprofile.site .
 cp ~/.Rprofile .
 cp ~/.bashrc .
 cp -r ~/Documents/Nvim/UltiSnips .
-cp ~/.config/redshift.conf .
+# cp ~/.config/redshift.conf .
 cp ~/.unison/default.prf .
 cp ~/.tmux.conf .
 cp -r ~/.tmux .
