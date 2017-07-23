67862f286c406230ce4c2c3d2c074a5699780e06
Erick Chacon <eral.th07@gmail.com>
2017-07-23 16:13:41 +0100

    pull and push for my dotfiles

--- pull.sh
+++ pull.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-echo "Working at home!"
-cd ~/Documents/Repositories/dotfiles-ubuntu/
-cp -r ~/.config/nvim .
-cp /etc/apt/sources.list .
-cp /etc/R/Rprofile.site .
-cp ~/.Rprofile .
-cp ~/.bashrc .
-cp -r ~/Documents/Nvim/UltiSnips .
-cp ~/.config/redshift.conf .
-cp ~/.unison/default.prf .
-cp ~/.tmux.conf .
-cp -r ~/.tmux .
-cp ~/.ctags .
-cp ~/.bash_it/aliases/custom.aliases.bash .
+if [[ $HOSTNAME == "chaconmo-ThinkPad-L470-W10DG" ]]; then
+  echo "Working at Lancaster University!"
+elif [[ $HOSTNAME == "chaconmo-Precision-5510" ]]; then
+  echo "Working at home!"
+fi
+
+# cd ~/Documents/Repositories/dotfiles-ubuntu/
+
+cp -r nvim ~/.config/
+# cp sources.list /etc/apt/sources.list
+# cp  Rprofile.site /etc/R/Rprofile.site
+cp .Rprofile ~/.Rprofile
+cp .bashrc ~/.bashrc
+cp -r UltiSnips ~/Documents/Nvim/
+# cp redshift ~/.config/redshift.conf
+cp default.prf ~/.unison/default.prf
+cp .tmux.conf ~/.tmux.conf
+cp -r .tmux ~/
+cp .ctags ~/.ctags
+cp custom.aliases.bash ~/.bash_it/aliases/custom.aliases.bash
--- push.sh
+++ push.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-echo "Working at home!"
+if [[ $HOSTNAME == "chaconmo-ThinkPad-L470-W10DG" ]]; then
+  echo "Working at Lancaster University!"
+elif [[ $HOSTNAME == "chaconmo-Precision-5510" ]]; then
+  echo "Working at home!"
+fi
+
 # cd ~/Documents/Repositories/dotfiles-ubuntu/
-# cd ~
-pwd
-cp -r nvim ~/.config/nvim
-cp -r UltiSnips ~/Documents/Nvim/UltiSnips
-# cp custom.aliases.bash ~/.bash_it/aliases/custom.aliases.bash
+
+cp -r ~/.config/nvim .
+cp /etc/apt/sources.list .
+cp /etc/R/Rprofile.site .
+cp ~/.Rprofile .
+cp ~/.bashrc .
+cp -r ~/Documents/Nvim/UltiSnips .
+cp ~/.config/redshift.conf .
+cp ~/.unison/default.prf .
+cp ~/.tmux.conf .
+cp -r ~/.tmux .
+cp ~/.ctags .
+cp ~/.bash_it/aliases/custom.aliases.bash .
