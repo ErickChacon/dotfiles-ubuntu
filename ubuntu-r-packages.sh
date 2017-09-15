5e046357c2f796408528725b1c6e72746f063227
Erick <eral.th07@gmail.com>
2017-09-15 05:06:33 +0100

    r packages script

--- ubuntu-r-packages.sh
+++ ubuntu-r-packages.sh
@@ -8,14 +8,17 @@ R CMD BATCH r-packages.R
 # models
 echo "install.packages(\"mgcv\")" > r-packages.R
 echo "install.packages(\"gamlss\")" >> r-packages.R
-echo "install.packages(\"bamlss\")" >> r-packages.R
-echo "install.packages(\"bamlss\", \
-  repos=\"http://R-Forge.R-project.org\")" >> r-packages.R
 echo "install.packages(\"R2BayesX\")" >> r-packages.R
 R CMD BATCH r-packages.R
-# echo "install.packages(\"bamlss\", repos = \"http://R-Forge.R-project.org\")" >> \
-  # r-packages.R
-# Warning: dependencies ‘coda’, ‘MBA’, ‘sp’, ‘spam’ are not available
+
+# bamlss
+echo "install.packages(\"coda\")" > r-packages.R
+echo "install.packages(\"MBA\")" >> r-packages.R
+echo "install.packages(\"sp\")" >> r-packages.R
+echo "install.packages(\"spam\")" >> r-packages.R
+echo "install.packages(\"bamlss\", \
+  repos=\"http://R-Forge.R-project.org\")" >> r-packages.R
+R CMD BATCH r-packages.R
 
 # tables
 echo "install.packages(\"gridExtra\")" > r-packages.R
