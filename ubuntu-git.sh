e3b173c20dfee3b6424434cbfd91fc255c6d76fe
Erick Chacon Montalvan <eral.th07@gmail.com>
2018-02-23 11:02:37 +0000

    text editor

--- ubuntu-git.sh
+++ ubuntu-git.sh
@@ -5,6 +5,7 @@ email=${2-"eral.th07@gmail.com"}
 
 git config --global user.name $user
 git config --global user.email $email
+git config --system core.editor vim
 ssh-keygen -t rsa -b 4096 -C $email
 ssh-add ~/.ssh/id_rsa
 
